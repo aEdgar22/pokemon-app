1f14388259c0c0258a620b00f5e32000291d0236
edgarElite <edgareliteapartments@gmail.com>
2023-03-24 16:06:04 -0500

    protected dashboard route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import Dashboard from "../pages/Dashboard";
+import PrivateRoute from './PrivateRoute';
 
 type Props = {};
 
 const AppRouter = (props: Props) => {
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path='*' element={<>NOT FOUND</>}/>
+        <Route path='/dashboard' element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }/>
       </Routes>
     </BrowserRouter>
   );
