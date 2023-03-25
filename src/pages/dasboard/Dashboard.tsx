724b271bf0a74eb0bec04dda1f3d8b3cadc1043f
edgar <a.edgar2209@gmail.com>
2023-03-25 10:51:17 -0500

    get the current logged in user

--- src/pages/dasboard/Dashboard.tsx
+++ src/pages/dasboard/Dashboard.tsx
@@ -1,11 +1,20 @@
 
 import NavBar from '../../components/NavBar';
+import { useAppDispatch } from '../../redux/hooks';
+import { setUserThunk } from '../../redux/thunks/setUserThunk';
 import PokeCardsLayout from './pokecards/PokeCardsLayout';
+import { useEffect } from 'react';
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
+  const dispatch = useAppDispatch()
 
+  useEffect(() => {
+    dispatch(setUserThunk())
+    
+  }, [dispatch])
+  
   return (
     <>
       <NavBar />
