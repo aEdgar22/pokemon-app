8f752773997de5ca34dba131f2f246bd891b4162
edgar <a.edgar2209@gmail.com>
2023-03-25 10:48:45 -0500

    updated pokemons interface

--- src/models/pokemons.model.ts
+++ src/models/pokemons.model.ts
@@ -1,5 +1,23 @@
+interface IPokemonSprites {
+    front_default: string;
+  }
+
+  interface IPokemonMoves {
+    move: {
+      name: string;
+    };
+  }
+  interface IPokemon {
+    id: number;
+    name: string;
+    weight: number;
+    url: string;
+    sprites: IPokemonSprites;
+    moves: IPokemonMoves[];
+  }
+
 export interface IPokemons{
     page: number,
-    pokemons: [],
+    pokemons: IPokemon[],
     isLoading: boolean
 }
