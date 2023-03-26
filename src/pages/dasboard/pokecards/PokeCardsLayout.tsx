88884838408acf02b0fc183ce2aeb581e7b3145c
edgar <a.edgar2209@gmail.com>
2023-03-26 00:52:51 -0500

    added state loading to pokecardLayout

--- src/pages/dasboard/pokecards/PokeCardsLayout.tsx
+++ src/pages/dasboard/pokecards/PokeCardsLayout.tsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import { getPokemons } from "../../../redux/thunks/pokemonThunk";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
-import { Button, Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import PokeCard from "./PokeCard";
 import { GridContainer } from "./styledComponents/GridContainerCards";
 import { IPokemons } from "../../../models/pokemons.model";
+import ButtonStyled from "./styledComponents/Button";
+import LazyLoad from "react-lazyload";
+import pikachuGif from "../../../assets/pikachu.gif";
 
 const PokeCardsLayout = () => {
   const dispatch = useAppDispatch();
@@ -14,6 +17,11 @@ const PokeCardsLayout = () => {
   );
 
   const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(isLoading);
+
+  useEffect(() => {
+    setLoading(isLoading);
+  }, [isLoading]);
 
   useEffect(() => {
     dispatch(getPokemons(page));
@@ -27,51 +35,80 @@ const PokeCardsLayout = () => {
     <Container
       maxWidth="xl"
       sx={{
-        height: "90vh",
+        height: "100%",
         marginTop: "2rem",
         padding: "1rem",
       }}
     >
-      <Typography
-        variant="h4"
-        sx={{
-          mr: 2,
-          textAlign: "center",
-          fontFamily: "poppins",
-          fontWeight: 700,
-          letterSpacing: ".3rem",
-          color: "inherit",
-          textDecoration: "none",
-        }}
-      >
-        POCKET MONSTERS
-      </Typography>
-
-      <GridContainer>
-        {pokemons.map((pokemon) => {
-          return <PokeCard key={pokemon.id} pokemon={pokemon} />;
-        })}
-      </GridContainer>
-
-      <Button
-        variant="outlined"
-        color="primary"
-        sx={{ margin: "4rem" }}
-        disabled={isLoading || page === 0}
-        onClick={() => handlePageChange(page - 1)}
-      >
-       -1
-      </Button>
-        Page {page + 1}
-      <Button
-        variant="outlined"
-        color="primary"
-        sx={{ margin: "4rem" }}
-        disabled={isLoading}
-        onClick={() => handlePageChange(page + 1)}
-      >
-        +1
-      </Button>
+      {loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection:"column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "300px",
+          }}
+        >
+         <img src={pikachuGif} alt="pikachu" />
+        </Box>
+      ) : (
+        <Typography
+          variant="h4"
+          sx={{
+            mr: 2,
+            textAlign: "center",
+            fontFamily: "poppins",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            textDecoration: "none",
+            fontSize: "2rem",
+            color: "#FFCB05",
+            textShadow: "2px 2px #4682b4c9",
+          }}
+        >
+          ¡POCKET MONSTERS!
+        </Typography>
+      )}
+      {!loading && (
+        <>
+          <GridContainer>
+            {pokemons.map((pokemon) => {
+              return (
+                <LazyLoad
+                  key={pokemon.id}
+                  height={200}
+                  once
+                  placeholder={<h1>Cargando...</h1>}
+                >
+                  <PokeCard pokemon={pokemon} />
+                </LazyLoad>
+              );
+            })}
+          </GridContainer>
+          <Box
+            margin={"2rem"}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            columnGap={"2rem"}
+          >
+            <ButtonStyled
+              disabled={loading || page === 0}
+              onClick={() => handlePageChange(page - 1)}
+            >
+              Before
+            </ButtonStyled>
+            Page {page + 1}
+            <ButtonStyled
+              disabled={loading}
+              onClick={() => handlePageChange(page + 1)}
+            >
+              Next
+            </ButtonStyled>
+          </Box>
+        </>
+      )}
     </Container>
   );
 };
