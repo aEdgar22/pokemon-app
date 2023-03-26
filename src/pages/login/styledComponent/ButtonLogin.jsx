bcc59b42bea1a45cd9b1eeac7e7977fa435a2216
edgar <a.edgar2209@gmail.com>
2023-03-26 00:53:42 -0500

    created styled button component for login

---
+++ src/pages/login/styledComponent/ButtonLogin.jsx
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+import ButtonStyled from "../../dasboard/pokecards/styledComponents/Button";
+
+export const ButtonLogin = styled(ButtonStyled)`
+    
+    width: 80%;
+    margin: 1rem 3rem;
+    background-color: #87CEEB;
+    color: #FFF;
+    border: 2px solid #87CEEB;
+    transition: background 0.5s ease-in-out;
+    font-size: 15px;
+    font-weight: bold;
+    letter-spacing: 1px; 
+
+  &:hover {
+    background-color: #FFF;
+    border: 2px solid #87CEEB;
+    color: #000000c4;
+
+  }
+
+
+`;
