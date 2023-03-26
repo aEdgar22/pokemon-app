722a5831c4a89a4524c28b5725772d49ae992d08
edgar <a.edgar2209@gmail.com>
2023-03-26 00:53:06 -0500

    created styled component button

---
+++ src/pages/dasboard/pokecards/styledComponents/Button.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const ButtonStyled = styled.button`
+  background-color: #0077cc;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+  
+  &:hover {
+    background-color: #005fa3;
+  }
+`;
+
+export default ButtonStyled;
